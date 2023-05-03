11becf019112aec7354d9e6a73cf516e0e27ed59
Renata Klepek <klepekrenata@gmail.com>
2023-05-03 19:52:57 +0200

    Ingredients update

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,35 +11,35 @@ export class RecipeService {
 
 
 private recipes: Recipe[] = [
-        new Recipe('Fleischspieße', 'Fleischspieße','../../../assets/img/img-1.jpg',
-        [new Ingredient('Fleisch', 1),
-        new Ingredient('Paprika', 1),
+        new Recipe('Beef Skewers', 'Beef Skewers','../../../assets/img/img-1.jpg',
+        [new Ingredient('Meat', 1),
+        new Ingredient('Pepper', 1),
         new Ingredient('Zucchini', 1),
-        new Ingredient('Champignons', 20)
+        new Ingredient('Mushroom', 20)
         ]),
-        new Recipe('Schwienefilet', 'Schwienefilet', '../../../assets/img/img-2.jpg',
-        [new Ingredient('Fleisch', 1),
-        new Ingredient('Spätzle', 1),
-        new Ingredient('Champignons', 1)
+        new Recipe('Pork Fillet', 'Pork Fillet', '../../../assets/img/img-2.jpg',
+        [new Ingredient('Meat', 1),
+        new Ingredient('Speatzle', 1),
+        new Ingredient('Mushroom', 10)
         ]),
-        new Recipe('Pfannkuchen', 'Süß oder Herzhaft', '../../../assets/img/img-3.jpg',
-        [new Ingredient('Eier', 2),
-        new Ingredient('Mehl', 1),
-        new Ingredient('Milch', 1),
-        new Ingredient('Salz', 1),
-        new Ingredient('Zucker', 1),
-        new Ingredient('Mineralwasser', 1)
+        new Recipe('Pancake', 'Sweet or Savory ', '../../../assets/img/img-3.jpg',
+        [new Ingredient('Egg', 2),
+        new Ingredient('Flour', 1),
+        new Ingredient('Milk', 1),
+        new Ingredient('Salt', 1),
+        new Ingredient('Sugar', 1),
+        new Ingredient('Mineral Water', 1)
         ]),
-        new Recipe('Rindersteak', 'Rindersteak', '../../../assets/img/img-4.jpg',
-        [new Ingredient('Rindersteak', 1),
-         new Ingredient('Kartoffel', 3),
-         new Ingredient('Pfifferlinge', 10)
+        new Recipe('Beef Steak', 'Beef Steak', '../../../assets/img/img-4.jpg',
+        [new Ingredient('Meat', 1),
+         new Ingredient('Potato', 3),
+         new Ingredient('Mushroom', 10)
         ]),
-        new Recipe('Torte', 'Geburtstagstorte', '../../../assets/img/img-5.jpg',
-        [new Ingredient('Eier', 3),
-          new Ingredient('Mehl', 1),
-         new Ingredient('Zucker', 1),
-         new Ingredient('Quark', 2)
+        new Recipe('Birthday Cake', 'Birthday Cake', '../../../assets/img/img-5.jpg',
+        [new Ingredient('Egg', 3),
+          new Ingredient('Flour', 1),
+         new Ingredient('Sugar', 1),
+         new Ingredient('Quark Cheese', 2)
         ])
 
       ];
