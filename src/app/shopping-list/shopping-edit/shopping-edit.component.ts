ffe4579fa364e3c9efec4c0b999e02439bff8b53
Renata Klepek <klepekrenata@gmail.com>
2023-05-03 17:02:23 +0200

    Allowing the Deletion of Shopping List Items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -49,6 +49,11 @@ this.slForm.reset();
 this.editMode = false;
 }
 
+onDelete(){
+this.slService.deleteIngredient(this.editedItemIndex);
+this.onClear();
+}
+
 ngOnDestroy(): void {
     this.subscription.unsubscribe();
 }
