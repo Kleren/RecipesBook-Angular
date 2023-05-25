f29435b90d3fab8fbfdb29ab305eb5f620627ed1
Renata Klepek <klepekrenata@gmail.com>
2023-05-25 20:11:59 +0200

    Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 import { throwError } from 'rxjs';
 
@@ -18,33 +18,47 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-return this.http.post<AuthResponseData>('',
-{
-  email: email,
-  password: password,
-  returnSecureToken: true
-}
-).pipe(catchError(errorRes => {
-  let errorMessage ='An unknow error occurred!';
+    return this.http
+    .post<AuthResponseData>('',
+      {
+        email: email,
+        password: password,
+      returnSecureToken: true
+      }
+    )
+    .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http
+    .post<AuthResponseData>('',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    )
+    .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage ='An unknow error occurred!';
   if (!errorRes.error || !errorRes.error.error) {
     return throwError(errorMessage);
   }
   switch (errorRes.error.error.message){
     case 'EMAIL_EXISTS':
       errorMessage = 'This email exists already';
+      break;
+    case 'EMAIL_NOT_FOUND':
+      errorMessage = 'This email does not exist.';
+      break;
+    case 'INVALID_PASSWORD':
+      errorMessage = 'This password is not correct.';
+      break;
+
   }
   return throwError(errorMessage);
-})
-);
-  }
-
-  login(email: string, password: string) {
-    return this.http.post<AuthResponseData>('',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true
-      }
-    );
-  }
+ }
 }
+
